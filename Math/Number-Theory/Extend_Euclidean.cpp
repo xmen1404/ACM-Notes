e19027ec36652f662fb5addb9505458d86778b5e
xmen1404 <40011636+xmen1404@users.noreply.github.com>
2021-02-20 17:05:08 +0700

    Update Extend_Euclidean.cpp

--- Math/Number-Theory/Extend_Euclidean.cpp
+++ Math/Number-Theory/Extend_Euclidean.cpp
@@ -1,11 +1,20 @@
-// Extended Euclidean without recursion
-pair<int, int> Extended_Euclidean(int a, int b) {
+pair<int, int> Extended_Euclidean_no_recursion(int a, int b) {
     int x = 1, y = 0, x1 = 0, y1 = 1; // xa + yb = a1 ... x1a + y1b = b1
     while(b) {
         int p = (a/b);
-        tie(x, x1) = mtp(x1, x - p * x1);
-        tie(y, y1) = mtp(y1, y - p * y1);
-        tie(a, b) = mtp(b, a - p * b);
+        tie(x, x1) = make_tuple(x1, x - p * x1);
+        tie(y, y1) = make_tuple(y1, y - p * y1);
+        tie(a, b) = make_tuple(b, a - p * b);
     }
     return {x, y};
+}
+
+int Extended_Euclidean(int a, int b, int &x, int &y) {
+    if(b == 0) {
+        x = 1, y = 0;
+        return a;
+    }
+    int _gcd = Extend_Euclidean(b, a % b, x, y);
+    tie(x, y) = make_tuple(y, x - y * (a/b));
+    return _gcd;
 }
