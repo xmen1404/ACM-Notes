91a1bab9fd42ff853258c3f87d231ca936eb846c
Nguyen Thanh Vinh <40011636+xmen1404@users.noreply.github.com>
2021-07-04 22:00:34 +0700

    Create Prime_checker.cpp

---
+++ Math/Prime_checker.cpp
@@ -0,0 +1,21 @@
+struct Prime_checker {
+    int n;
+    vector<bool> prime;
+    vector<int> plist;
+
+    Prime_checker(int n = 0): n(n) {
+        prime.assign(n + 10, true);
+    }
+    void find_prime() {
+        for(int i = 4; i <= n; i += 2) 
+            prime[i] = false;
+        for(int i = 3; i * i <= n; i += 2) 
+            if(prime[i]) {
+                for(int j = i * i; j <= n; j += i) 
+                    prime[j] = false;
+            }
+        FOR(i, 2, n) if(prime[i]) 
+            plist.pb(i);
+    }
+    // now we have prime[x] = check if x is a prime number
+} tmp;
