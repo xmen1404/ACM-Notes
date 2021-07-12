d27e8bedfaeb7ef600a5c6b1c3e2749634bb630e
Nguyen Thanh Vinh <40011636+xmen1404@users.noreply.github.com>
2021-07-12 17:25:11 +0700

    Create miller_rabin_prime_check.cpp

---
+++ Math/miller_rabin_prime_check.cpp
@@ -0,0 +1,42 @@
+// Improved version of Fermat prime check (iter >= 25 is enough)
+// n = 2^s * d (d odd)
+// Check for a random base A, if A^d % n == 1 or A^d % n == -1
+// if both not satisfy => composite
+// 75% prob that a random base will test correctly if n composite
+
+int pw(int x, int y, int mod) {
+    int res = 1;
+    while(y) {
+        if(y%2) res = res * x % mod;
+        x = x*x%mod;
+        y >>= 1;
+    }
+    return res;
+}
+bool check_prime(int n, int A, int s, int d) {
+    int x = pw(A, d, n);
+    if(x == 1 || x == (n - 1)) return true;
+    FOR(r, 1, s - 1) {
+        x = (x * x) % n;
+        if(x == (n - 1)) return true;
+    }
+    return false;
+}
+bool miller_rabin_prime_check(int n) { // int64
+    if(n < 4) return (n == 2 || n == 3);
+
+    int s = 0, tmp_n = n - 1;
+    while(tmp_n % 2 == 0) {
+        s += 1;
+        tmp_n /= 2;
+    }
+    int d = (n / (1ll << s));
+    // iter = 25 -> prob of failing = 1e-16
+    FOR(i, 1, 25) {
+        int A = 2 + rand() % (n-3);
+        if(!check_prime(n, A, s, d)) {
+            return false;
+        }
+    }
+    return true;
+}
