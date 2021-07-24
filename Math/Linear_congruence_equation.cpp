91e613a321b038dfd79227e6d23030535dc8ce81
Nguyen Thanh Vinh <40011636+xmen1404@users.noreply.github.com>
2021-07-24 17:42:41 +0700

    Update Linear_congruence_equation.cpp

--- Math/Linear_congruence_equation.cpp
+++ Math/Linear_congruence_equation.cpp
@@ -1,1 +1,8 @@
 // solution for eq: ax % m = b (x = unknown)
+// if gcd(a, m) == 1 -> unique solution: x = b * inv(a) 
+// if gcd(a, m) == p > 1 -> (a/p) * x % (m/p) = (b/p)
+//      if b % p != 0 -> no solution
+//      else (a/p) and (m/p) coprime -> solution x = x' (1st case)
+//          there are p solutions: x = x' + k * (m/p) with k in [0, p-1]
+
+// Other solution: Solve Diophantine equation: ax + k*m = b (x, k = unknowns)
