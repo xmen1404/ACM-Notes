94f68769d28e42f7a1589df81c0492fea57c7f7e
Nguyen Thanh Vinh <40011636+xmen1404@users.noreply.github.com>
2021-07-25 10:49:27 +0700

    Create Chinese_remainder_theorem.cpp

---
+++ Math/Chinese_remainder_theorem.cpp
@@ -0,0 +1,5 @@
+// Chinese remainder theorem (CRT)
+// p = p1 * p2 * ... * pk (pi pairwise relative prime)
+// CRT: for each a < p -> express uniquely as system of modulo eq: a % pi = ai (and conversely)
+// Use Garner's algorithm to find a from system
+// O(k^2)
